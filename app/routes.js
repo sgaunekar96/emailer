1e722e2e83cfebdff06fddf01a533b90e9d940e5
Soham Gaunekar <sgaunekar96@gmail.com>
2016-02-02 04:14:46 +0530

    initial commit

---
+++ app/routes.js
@@ -0,0 +1,51 @@
+// app/routes.js
+module.exports = function(app, passport) {
+
+    app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+
+    app.get('/login', function(req, res) {
+
+        res.render('login.ejs', { message: req.flash('loginMessage') }); 
+    });
+
+    app.get('/signup', function(req, res) {
+
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile',
+        failureRedirect : '/signup',
+        failureFlash : true 
+    }));
+    
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile',
+        failureRedirect : '/login',
+        failureFlash : true
+    }));
+
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
